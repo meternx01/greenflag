1bbbf90f4cbafdb9b9c9548a152f816ddc0e0ad6
Jason McKinney <meter_1@yahoo.com>
2019-07-27 12:46:19 -0400

    Finished Main Activity

--- app/src/main/java/com/example/greenflag/MainActivity.java
+++ app/src/main/java/com/example/greenflag/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.greenflag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rl_main;
+    Button btn_ma_create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rl_main = (RelativeLayout) findViewById(R.id.rl_main);
+        btn_ma_create = (Button) findViewById(R.id.btn_ma_create);
+
+        btn_ma_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getBaseContext(),
+                        EmailActivity.class
+                );
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
+
 }
