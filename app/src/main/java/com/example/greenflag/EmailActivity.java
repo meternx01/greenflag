638d758ac8570b354af4c254d71ac4cd83b99c94
Jason McKinney <meter_1@yahoo.com>
2019-07-28 09:45:39 -0400

    Finished Layout Email / Password Layout

--- app/src/main/java/com/example/greenflag/EmailActivity.java
+++ app/src/main/java/com/example/greenflag/EmailActivity.java
@@ -2,13 +2,52 @@ package com.example.greenflag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class EmailActivity extends AppCompatActivity {
+
+    RelativeLayout rl_emailpass;
+    ImageButton ib_ea_back;
+    Button btn_ea_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
+
+        rl_emailpass = (RelativeLayout) findViewById(R.id.rl_emailpass);
+        ib_ea_back = (ImageButton) findViewById(R.id.ib_ea_back);
+        btn_ea_next = (Button) findViewById(R.id.btn_ea_next);
+
+        ib_ea_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getBaseContext(),
+                        MainActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+
+        btn_ea_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getBaseContext(),
+                        ProfileActivity.class
+                );
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
